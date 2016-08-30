e9228d4d3b2f853cc5b3667053f8034c2e4c7e94
catkinson <catkinson@bloomhealthco.com>
2016-08-30 10:55:13 -0500

    Adding test for @IgnoreIf

---
+++ src/test/groovy/example/ignore/IgnoreIfSpec.groovy
@@ -0,0 +1,15 @@
+package example.ignore
+
+import spock.lang.IgnoreIf
+import spock.lang.Specification
+
+class IgnoreIfSpec extends Specification {
+
+    // To run this test, execute:
+    // ./gradlew test -Drun.failing=true
+    @IgnoreIf({ Boolean.valueOf(properties['run.failing']) })
+    def 'should only run if system property is set'() {
+        expect:
+        assert false
+    }
+}
